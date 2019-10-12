6dfcb738c4f934b788188cf726a4113f26aa729e
codrelphi <chancerel.codjovi@gmail.com>
2019-10-12 16:43:52 +0100

    write the code

--- day0.js
+++ day0.js
@@ -4,3 +4,11 @@
  date: 2019-10-12
  source: https://www.hackerrank.com/challenges/js10-hello-world/problem
 ================================================================================= */
+
+function greeting(parameterVariable) {
+    // print 'Hello World!'
+    console.log('Hello, World!');
+    // print the content of parameterVariable
+    console.log(parameterVariable);
+
+}
