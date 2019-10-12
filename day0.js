26fc9eb7a4c517660db4e2cf1687e2022857b3a6
codrelphi <chancerel.codjovi@gmail.com>
2019-10-12 16:42:52 +0100

    create the file and add my personal header

---
+++ day0.js
@@ -0,0 +1,6 @@
+/*
+=================================================================================
+ author: Chancerel Codjovi (aka codrelphi)
+ date: 2019-10-12
+ source: https://www.hackerrank.com/challenges/js10-hello-world/problem
+================================================================================= */
