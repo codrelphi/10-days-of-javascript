a97f8b88b48f199c53e0298c7d13286e2c1a136f
codrelphi <chancerel.codjovi@gmail.com>
2019-10-13 23:56:49 +0100

    create the file day1_0.js and add my personal header

---
+++ day1_0.js
@@ -0,0 +1,6 @@
+/*
+=================================================================================
+ author: Chancerel Codjovi (aka codrelphi)
+ date: 2019-10-13
+ source: https://www.hackerrank.com/challenges/js10-data-types/problem
+================================================================================= */
